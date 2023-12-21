27d2a8ff92690b51ff42eaafef877dd51a893121
VasilyevLM <vasilyevlm@gmail.com>
2023-12-21 19:10:01 +0300

    dsfvdfsv

--- game.py
+++ game.py
@@ -1,38 +1,14 @@
 import random
+import string
 
-# def kick(list_war):
-#     active = random.choice(list_war)
-#     damage = random.randint(1, 20)
-#     hp = active["hp"]
-#     hp -= damage
-#     active["hp"] = hp
-#     if active["name"] == "Первый воин":
-#         list_war[0] = active
-#     else:
-#         list_war[1] = active
-#     return list_war
-#
-#
-# list_war = [
-#     {"name": "Первый воин", "hp": 100},
-#     {"name": "Второй воин", "hp": 100}
-# ]
-#
-#
-# while list_war[0]["hp"] > 0 and list_war[1]["hp"] > 0:
-#     hit = random.choice([True, False])
-#     if hit:
-#         list_war = kick(list_war)
-#         print(list_war)
-#     else:
-#         print('не удачно')
 
 
 class Attribute:
-    def __init__(self, name, hp, damage):
+    def __init__(self, name: str, hp: int, damage: int, flag: str) -> None:
         self.name = name
         self.hp = hp
         self.damage = damage
+        self.flag = flag
 
 
 
@@ -46,26 +22,49 @@ class Attribute:
             self.hp = 0
         return self.hp
 
+    def team(self):
+        return self.flag
 
-
-warrior1 = Attribute(name='Harald', hp=100, damage=random.randint(1, 20))
-warrior2 = Attribute(name='Gustav', hp=100, damage=random.randint(1, 20))
-
-
+warriors = []
+flags = ['red', 'blue']
+names = []
 hit = random.choice([True, False])
 
-while warrior1.hp > 0 and warrior2.hp > 0:
-    if hit:
-        active = random.choice([warrior1, warrior2])
-        if active.name == 'Harald':
-            print('Воин', warrior1.name, 'наносит', warrior1.damage, 'урона', warrior2.name, warrior2.loss(damage=warrior1.kick()))
-        else:
-            print('Воин', warrior2.name, 'наносит', warrior2.damage, 'урона', warrior1.name, warrior1.loss(damage=warrior2.kick()))
 
-if warrior1.hp > 0:
-    print('Выиграл', warrior1.name)
-else:
-    print('Выиграл', warrior2.name)
+for i in range(10000):
+    warriors.append(Attribute(name=''.join((random.choice(string.ascii_lowercase) for x in range(10))), hp=100, damage=random.randint(1, 20), flag=random.choice(flags)))
+
+for i in warriors:
+    print(i.name, i.flag)
+
+
+active1 = random.choice(warriors)
+active2 = random.choice(warriors)
+
+while (active2.flag != active1.flag) and (active1.hp > 0 or active2.hp <= 0):
+
+    print('Воин', active1.name, 'наносит', active2.damage, 'урона', active2.name, active2.loss(damage=active1.kick()))
+
+
+
+
+
+
+
+
+# while warrior1.hp > 0 and warrior2.hp > 0:
+#     if hit:
+#         if warrior1.flag != warrior2.flag:
+#             active = random.choice([warrior1, warrior2])
+#             if active.name == 'Harald':
+#                 print('Воин', warrior1.name, 'наносит', warrior1.damage, 'урона', warrior2.name, warrior2.loss(damage=warrior1.kick()))
+#             else:
+#                 print('Воин', warrior2.name, 'наносит', warrior2.damage, 'урона', warrior1.name, warrior1.loss(damage=warrior2.kick()))
+#
+# if warrior1.hp > 0:
+#     print('Выиграл', warrior1.name)
+# else:
+#     print('Выиграл', warrior2.name)
 
 
 
