e663f026cfa701365969e76d5d34cba05af3d4c6
VasilyevLM <vasilyevlm@gmail.com>
2024-01-18 19:34:15 +0300

    rgdegsdgdasdasd

--- parser.py
+++ parser.py
@@ -2,76 +2,137 @@ from bs4 import BeautifulSoup
 import requests
 import xlsxwriter
 import re
+import openpyxl
 
-# class Competitors:
-#     def __init__(self, site, sitemap, name, sitemap_url):
-#         self.name = name
-#         self.site = site
-#         self.sitemap = sitemap
-#         self.sitemap_url = sitemap_url
-
-
-domain = 'https://forintek.ru/catalog/printer-etiketok-snbc-btp-7400/'
-# sitemap = 'sitemap.xml'
-
-sitemap_url = domain
-r = requests.get(sitemap_url)
+link = 'https://forintek.ru/catalog/printer-etiketok-snbc-btp-7400/'
 # print(r.status_code)
 # print(r.text)
-soup = BeautifulSoup(r.text, 'lxml')
-
-a = soup.find_all('h1')
-# print(a)
-xml_list = []
-
-for i in a:
-    xml_list.append(i.text)
-    print(i)
-
-# b = soup.find_all('td', class_='font-weight-bold')
-c = soup.find_all('td')
-
-attrs = []
-counter = 1
-
-# for i in b:
-#     attrs[f'parameter{counter}'] = i.text
-#     counter += 1
-#     print(i)
-
-for i in c:
-    attrs.append(i.text)
-    print(i.text)
 
 
+class Competitors:
+    # def __init__(self, counter, article, name, print_method, resolution, print_width, print_speed, memory, interface, old_shtrikh, new_shtrikh, paper_type, paper_roll_width, paper_thicknes, roll_outer_diameter, sleeve_diameter, input_signal, tape_length, tape_sleeve_diameter, operating_mode, storage_condition, size, weight, link):
+        # self.article = article
+        # self.name = name
+        # self.print_method = print_method
+        # self.resolution = resolution
+        # self.print_width = print_width
+        # self.print_speed = print_speed
+        # self.memory = memory
+        # self.interface = interface
+        # self.old_shtrikh = old_shtrikh
+        # self.new_shtrikh = new_shtrikh
+        # self.paper_type = paper_type
+        # self.paper_roll_width = paper_roll_width
+        # self.paper_thicknes = paper_thicknes
+        # self.roll_outer_diameter = roll_outer_diameter
+        # self.sleeve_diameter = sleeve_diameter
+        # self.input_signal = input_signal
+        # self.tape_length = tape_length
+        # self.tape_sleeve_diameter = tape_sleeve_diameter
+        # self.operating_mode = operating_mode
+        # self.storage_condition = storage_condition
+        # self.size = size
+        # self.weight = weight
+        # self.link = link
+        # self.counter = counter
+
+    def __init__(self):
+        self.attrs = []
+
+    def steal(self):
+        r = requests.get(link)
+        soup = BeautifulSoup(r.text, 'lxml')
+        name = soup.find_all('h1')
+        name = name[0].text
 
 
-workbook = xlsxwriter.Workbook('forintek.xlsx')
-worksheet = workbook.add_worksheet()
-for i in range(10):
-    for j in range(5):
-        worksheet.write(i, j, i+j)
-workbook.close()
+        b = soup.find_all('th')
+
+
+        article = b[1].text
+
+        self.attrs.append('name')
+        self.attrs.append(article)
+        self.attrs.append('name')
+        self.attrs.append(name)
+
+
+        c = soup.find_all('td')
+
+
+        for i in c:
+            self.attrs.append(i.text)
+
+        self.attrs.append('dsdd')
+        self.attrs.append(link)
 
 
 
 
 
 
-# print(xml_list)
-#
-# for i in xml_list:
-#     print(i)
-#     try:
-#         b = requests.get(i)
-#         soup1 = BeautifulSoup(b.text, 'lxml')
-#         h1 = soup1.find('h3')
-#         print(h1.text)
-#         with open('h1.txt', mode='a', encoding='utf-8') as file:
-#             file.write(f'{h1.text}\n{i}\n')
-#
-#     except Exception as err:
-#         print(err)
+
+
+
+
+    def excel(self):
+        workbook = xlsxwriter.Workbook('forintek.xlsx')
+        worksheet = workbook.add_worksheet()
+        worksheet.write(0, 0, 'Артикул')
+        worksheet.write(0, 1, 'Наименование')
+        worksheet.write(0, 2, 'Метод печати')
+        worksheet.write(0, 3, 'Разрешение')
+        worksheet.write(0, 4, 'Ширина печати')
+        worksheet.write(0, 5, 'Скорость печати')
+        worksheet.write(0, 6, 'Память')
+        worksheet.write(0, 7, 'Интерфейс')
+        worksheet.write(0, 8, '1D штрихкодов')
+        worksheet.write(0, 9, '2D штрихкодов')
+        worksheet.write(0, 10, 'Тип бумаги')
+        worksheet.write(0, 11, 'Ширина рулона бумаги')
+        worksheet.write(0, 12, 'Толщина бумаги')
+        worksheet.write(0, 13, 'Внешний диаметр рулона')
+        worksheet.write(0, 14, 'Диаметр втулки')
+        worksheet.write(0, 15, 'Входной сигнал')
+        worksheet.write(0, 16, 'Длина ленты')
+        worksheet.write(0, 17, 'Диаметр втулки ленты')
+        worksheet.write(0, 18, 'Эксплуатационный режим')
+        worksheet.write(0, 19, 'Условия хранения')
+        worksheet.write(0, 20, 'Размеры')
+        worksheet.write(0, 21, 'Вес')
+        worksheet.write(0, 22, 'Ссылка')
+        worksheet.set_default_row(22)
+
+        bold = workbook.add_format({'bold': True})
+        worksheet.set_row(0, 22, bold)
+        worksheet.set_column('A:T', 22)
+        worksheet.freeze_panes(1, 0)
+
+        workbook.close()
+
+    def safe_date(self):
+        wb = openpyxl.load_workbook('forintek.xlsx')
+        sheet = wb.active
+
+
+        x = 1
+        for index_i, i in enumerate(self.attrs):
+
+            if index_i % 2 != 0:
+                sheet.cell(row=2, column=x).value = i
+                x += 1
+        wb.save('forintek.xlsx')
+
+
+
+
+
+x = Competitors()
+x.steal()
+x.excel()
+x.safe_date()
+
+
 
 
 
