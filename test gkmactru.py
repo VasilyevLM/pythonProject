285cd5ec5d1660912f8d9b8d510259a193cf98d4
Леонид <vasilyevlm@gmail.com>
2024-04-04 19:01:30 +0300

    dfdsfd

--- test gkmactru.py
+++ test gkmactru.py
@@ -4,18 +4,20 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 
+options = Options()
+options.page_load_strategy = 'eager'
 
-
-browser = webdriver.Chrome()
+browser = webdriver.Chrome(options=options)
 browser.get('https://gk-mact.ru')
 
 title = browser.title
 print(title)
-# coockie = browser.find_element(By.XPATH, '/html/body/div[8]/div/div/div[3]/span[1]').click()
+
 submit = browser.find_element(By.XPATH, '/html/body/div[4]/div[6]/div[2]/div/div/div[2]/div[3]/div[2]/div/div/form/div[3]/div/button')
 browser.execute_script('arguments[0].scrollIntoView();', submit)
-# browser.execute_script("window.scrollTo(5, 2500)")
+
 time.sleep(5)
 
 text_box = browser.find_element(By.XPATH, '/html/body/div[4]/div[6]/div[2]/div/div/div[2]/div[3]/div[2]/div/div/form/div[2]/div[1]/div[1]/input')
@@ -23,13 +25,47 @@ text_box2 = browser.find_element(By.XPATH, '/html/body/div[4]/div[6]/div[2]/div/
 
 text_box.send_keys('Test')
 text_box2.send_keys('9201337011')
-time.sleep(3)
+time.sleep(1)
 
 submit.click()
 
+time.sleep(1)
+
+time.sleep(1)
+browser.execute_script("window.scrollTo(5, 3500)")
 time.sleep(3)
-browser.close()
-browser.quit()
+product = browser.find_element(By.XPATH, '/html/body/div[4]/div[6]/div[2]/div/div/div[6]/div/div/div/div/ul/li[1]/div/div[1]/div/div[2]/div[1]/div[2]/div/div[1]')
+browser.execute_script('arguments[0].scrollIntoView();', product)
+time.sleep(3)
+product.click()
+time.sleep(3)
+browser.execute_script("window.scrollTo(5, 500)")
+time.sleep(3)
+buy = browser.find_element(By.XPATH, '/html/body/div[4]/div[6]/div[2]/div/div/div/div[2]/div[1]/div/div/div/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div[2]/div[1]/div[2]/span[1]')
+browser.execute_script('arguments[0].scrollIntoView();', buy)
+time.sleep(3)
+buy.click()
+# time.sleep(3)
+# basket = browser.find_element(By.XPATH, '/html/body/div[7]/div/div/div/div[1]/div[1]/div/div/span')
+# basket.click()
+time.sleep(3)
+basket2 = browser.find_element(By.XPATH, '/html/body/div[7]/div/div/div/form/ul/li/div[1]/div[3]/div[2]/div[1]/a')
+basket2.click()
+time.sleep(3)
+order = browser.find_element(By.XPATH, '/html/body/div[4]/div[6]/div[2]/div/div/div[2]/div/div[1]/div[1]/div/div/div[2]/div/div[3]/div/div[1]/button')
+order.click()
+time.sleep(10)
+
+
+
+
+
+
+
+
+
+# browser.close()
+# browser.quit()
 
 
 # browser.execute_script("window.scrollTo(0, document.body.scrollHeight)")
